5bb355aa1fc74f6c5c99138212fa0f2e6c2c6707
Devang Patel <devangminer722@gmail.com>
2021-03-02 21:08:57 -0500

    made the intro for day 3

---
+++ day3/day3_runner.py
@@ -0,0 +1,40 @@
+'''
+object oriented python
+'''
+
+'''
+What is object oriented programming?
+-it is structuring you programming so that you are budling related properties and behaviors into objects
+-this won't be a OOP toutorial, but a OOP in python tutorial
+'''
+
+# you could represetnt some data for some items data you need to sore, but it is clunky
+# we can use objects instead and in python it is similar to how you would do it in other lanuages
+monster = ['orc', 23, 'guard', 4]
+human = ['human', 14, 'knight', 6]
+
+'''
+
+
+note to self make all this some powerpoint slides later
+
+
+first some keywords:
+
+class 
+- this is how you define a class in python
+- like in other languages class is the 'blueprint' you use to build an object
+- angain like in other languages an instance of an object means that the blueprint has been used to make an object and you are using it
+- this keyword is followed by the name of the class then a ':' similar to how in other languages you use {}
+
+__init__()
+- this is what in other languages is the constuctor method
+
+self
+- this the same as using 'this' in java
+- every method in an object's class has to have this as the first parameter
+- when you call it you don't use self, you use it when you make the class 
+- this included the __init__() too
+
+now lets move to a new file that we will called
+'''
