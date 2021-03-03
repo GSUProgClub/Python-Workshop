22c20d7288c09e3bcf7b6b4ba98c0c9f67666aae
Devang Patel <devangminer722@gmail.com>
2021-03-02 21:22:50 -0500

    added more keywords to day 3 intro

--- day3/day3_runner.py
+++ day3/day3_runner.py
@@ -14,8 +14,6 @@ monster = ['orc', 23, 'guard', 4]
 human = ['human', 14, 'knight', 6]
 
 '''
-
-
 note to self make all this some powerpoint slides later
 
 
@@ -36,5 +34,14 @@ self
 - when you call it you don't use self, you use it when you make the class 
 - this included the __init__() too
 
-now lets move to a new file that we will called
+__str__
+- this is how you over ride the string fuction of the object
+- without this when you go to print your object it would output the memeory adress of the object
+- this is effivly overriding the same as '.toSring()' in java
+
+__add__
+- this how you over ride the '+' of python so you can esily add to object without makeing a differnt method
+
+
+now lets move to a new file that we will called 'monster.py'
 '''
